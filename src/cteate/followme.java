5dc7ad85145a06f94a71e3efc66b472a8b3cc4dd
Sarith <sarithkak2510@gmil.com>
2021-07-31 16:26:46 +0700

    This my Class

---
+++ src/cteate/followme.java
@@ -0,0 +1,8 @@
+package cteate;
+
+public class followme {
+    public static void main(String[] args){
+        //This is show console Hello world!
+        System.out.println("Hello World");
+    }
+}
--- src/lession/Selection.java
+++
@@ -1,8 +0,0 @@
-package lession;
-
-public class Selection {
-    public static void main(String[] args){
-        //This is show console Hello!
-        System.out.println("Hello");
-    }
-}
